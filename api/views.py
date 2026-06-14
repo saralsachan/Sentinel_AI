a0171aeb490b3c817b2206c8b51610e1a9efb002
Saral Sachan <saralsachan06@gmail.com>
2026-06-14 14:17:54 +0530

    Added views.py

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import CameraNode, Incident
+from .serializers import CameraNodeSerializer, IncidentSerializer
 
-# Create your views here.
+class CameraNodeViewSet(viewsets.ModelViewSet):
+    queryset = CameraNode.objects.all()
+    serializer_class = CameraNodeSerializer
+    # This locks the endpoint. No valid JWT = 401 Unauthorized.
+    permission_classes = [IsAuthenticated]
+
+class IncidentViewSet(viewsets.ModelViewSet):
+    queryset = Incident.objects.all().order_by('-timestamp') # Newest first
+    serializer_class = IncidentSerializer
+    permission_classes = [IsAuthenticated]
